06030adf9a14d28702b2a51b2d69b95969b4a260
dashaivasha <71345093+dashaivasha@users.noreply.github.com>
2022-05-18 15:44:07 +0300

    Add files via upload

---
+++ Boosting/PageObjects/BasePage.cs
@@ -0,0 +1,18 @@
+﻿using OpenQA.Selenium;
+using Boosting.Driver;
+using Boosting.TestData;
+
+namespace Boosting.PageObjects
+{
+    public class BasePage
+    {
+        protected IWebDriver Driver;
+        internal TestDetails Data = JsonManager.GetTestData();
+
+        public BasePage()
+        {
+            DriverFactory.InitalizerDriver();
+            Driver = DriverFactory.Driver;
+        }
+    }
+}
---
+++ Boosting/Tests/UITests/IFramesTest.cs
@@ -0,0 +1,23 @@
+﻿using Boosting.Tests.UiTests;
+using NUnit.Framework;
+
+namespace Boosting.Tests.UITests
+{
+    [TestFixture]
+    internal class IFramesTest : BaseTest
+    {
+        [SetUp]
+        public void Open()
+        {
+            WebDriver.Navigate().GoToUrl(Data.Url);
+        }
+
+        [Test]
+        public void OpenTwoTabs()
+        {
+            demoQa.Switch();
+            demoQa.SwitchToDefaultContent();
+            Assert.IsTrue(demoQa.IsItDefaultContent());
+        }
+    }
+}
